63fc5f15894eeedc0f168fef1b708e2e72e8406d
kindOfCurly <marie.sautmann@gmx.de>
2018-08-09 15:53:45 +0200

    database new setup

--- app/src/main/java/com/example/isibell/dbprojekt/DriverActivity.java
+++ app/src/main/java/com/example/isibell/dbprojekt/DriverActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class FahrerActivity extends AppCompatActivity {
+public class DriverActivity extends AppCompatActivity {
     private Button getOrderButton, orderDoneButton;
     private TextView targetPositionView;
     private int PersNr;
